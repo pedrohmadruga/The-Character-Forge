d873a3d839e7bed5bb6e93fc56063f705d6b5f0a
pedrohmadruga <pedrohfonseca01@gmail.com>
2023-05-14 16:17:54 -0300

    Botão de busca funcionando

--- main_site/views.py
+++ main_site/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView, ListView, DetailView
 from . import models
+from django.db.models import Q
 
 
 class MainPageView(TemplateView):
@@ -11,6 +12,22 @@ class OriginalBuildsView(ListView):
     template_name = 'original_builds.html'
     context_object_name = 'personagens'
     paginate_by = 3
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        name = self.request.GET.get('name')
+        race = self.request.GET.get('race')
+        classe = self.request.GET.get('class')
+
+        if name or race or classe:
+            queryset = queryset.filter(
+                Q(nome__icontains=name) if name else Q(),
+                Q(raca__icontains=race) if race else Q(),
+                Q(classe__icontains=classe) if classe else Q()
+            )
+
+        return queryset
 
 
 class PersonagemOriginalDetailView(DetailView):
